260cfdc015e2afe71205f813867d34ad41d508fb
bristouflex <b.hounza@laposte.net>
2017-05-14 20:46:24 +0200

    modification of meteo

--- meteo/main.c
+++ meteo/main.c
@@ -21,36 +21,35 @@ char* getDate(){
 
 }
 
-//la fonction fait le curl sur l'api pour la météo et renvoie la chaine contenant son resultat
-char *getInformations(){
+//la fonction fait le curl sur l'api pour la météo et renvoie la string contenant son resultat
+char *getInformations(char *url){
 	FILE *file;
-	char *chaine;
+	char *string;
     char buffer[2000];
-
-	chaine = malloc(sizeof(char)*5000);
-	strcpy(chaine, "");
-	file = popen("curl \"http://api.openweathermap.org/data/2.5/weather?id=3019265&appid=4530f696e6e24c57b4ed248cdce5b64b&lang={fr}\"", "r");
+	string = malloc(sizeof(char)*5000);
+	strcpy(string, "");
+	file = popen(url, "r");
 	if(file == NULL){
             exit(-1);
 	}
 	while(fgets(buffer, 2000, file) != NULL){
-        strcat(chaine, buffer);
+        strcat(string, buffer);
 	}
 	pclose(file);
-	return chaine;
+	return string;
 }
 
-//la fonction prend la requete en paramètre et l'écrit en bdd
-void writeInBdd(char requete[5000]){
+//la fonction prend la request en paramètre et l'écrit en bdd
+void writeInBdd(char request[5000]){
     MYSQL mysql; //Déclaration du pointeur de structure de type MYSQL
 
     mysql_init(&mysql); //Initialisation de MySQL
-    printf("%s", requete);
+    printf("%s", request);
 
 	if(mysql_real_connect(&mysql,"127.0.0.1","root","","pds",0,NULL,0))
     {
 
-        mysql_query(&mysql, requete);
+        mysql_query(&mysql, request);
         printf("\nok");
         mysql_close(&mysql); //Fermeture de MySQL
     }
@@ -62,34 +61,40 @@ void writeInBdd(char requete[5000]){
 
 int main(int argc, char const *argv[])
 {
-    char resultats[7][15] = {"", "", "", "", "", "", ""};
-    char recherches[14][20]= {"description\":\"", "\"", "temp\":", ",", "pressure\":", ",","humidity\":", ",", "speed\":", ",", "deg\":", "}", "all\":", "}" };
-    int i;
+    char results[7][15] = {"", "", "", "", "", "", ""};
+    char research[14][20]= {"description\":\"", "\"", "temp\":", ",", "pressure\":", ",","humidity\":", ",", "speed\":", ",", "deg\":", "}", "all\":", "}" };
+    int i, k;
     int j = 0;
-    char* chaineB;
-    char* texte;
-    char requete[5000];
+    char* stringB;
+    char* text;
+    char request[5000];
+    char info[500] = "";
     char* date;
+    FILE *file;
+
+    file = fopen("conf.txt", "rb");
+    fgets(info, 2000, file);
+    text = getInformations(info);
 
     date = getDate();
-    texte = getInformations();
     for(i = 0; i < 14; i++){
         if(i %2 != 0){
             if(i == 1){
-                chaineB = strstr(texte+strlen(recherches[0]), recherches[i]);
+                stringB = strstr(text+strlen(research[0]), research[i]);
             }else{
-                chaineB = strstr(texte, recherches[i]);
+                stringB = strstr(text, research[i]);
             }
-            strncpy(resultats[j], texte+strlen(recherches[i-1]), chaineB-texte-strlen(recherches[i-1]));
-            printf("%s\n", resultats[j]);
+            strncpy(results[j], text+strlen(research[i-1]), stringB-text-strlen(research[i-1]));
             j++;
         }else{
-            texte = strstr(texte, recherches[i]);
+            text = strstr(text, research[i]);
         }
     }
-    sprintf(requete, "INSERT INTO meteo(description, temperature, pressure, humidity, vitesse, inclinaison, precipitations, date) VALUES(\'%s\', %s, %s, %s, %s, %s, %s, \'%s\')", resultats[0], resultats[1], resultats[2], resultats[3], resultats[4], resultats[5], resultats[6], date);
-    writeInBdd(requete);
+
+    sprintf(request, "INSERT INTO meteo(description, temperature, pressure, humidity, vitesse, inclinaison, precipitations, date) VALUES(\'%s\', %s, %s, %s, %s, %s, %s, \'%s\')", results[0], results[1], results[2], results[3], results[4], results[5], results[6], date);
+    writeInBdd(request);
     free(date);
-	free(texte);
+	free(text);
+	fclose(file);
 	return 0;
 }
